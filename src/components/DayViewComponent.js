b0112e18e4338e55077da5aa212b273733223836
Bruk Berhane Asfaw <brukberhane@hotmail.com>
2020-03-05 09:06:32 +0300

    Scrolling to specific day is now working on the Windows ScheduleScreen. Also trying a different way to see the current day of the week by putting it inside the FlatList and having it scroll with it. Reworked Card to make it even looking instead of the old style I got from the album proj. Times are now in ranges rather than just showing starting times.

--- src/components/DayViewComponent.js
+++ src/components/DayViewComponent.js
@@ -2,17 +2,44 @@ import React from 'react';
 import { ScrollView, View, Text, StyleSheet } from 'react-native';
 import Period from './PeriodView';
 
-const DayViewComponent = ({ day }) => {
-    const { containerStyle, headerTextStyle } = styles;
+const DayViewComponent = ({ day, dayNum }) => {
+    const { dayStyle } = styles;
+
+    let dayStr = "";
+    console.log("INdex: " + dayNum)
+    switch (dayNum) {
+        case 0:
+            dayStr = "Monday";
+            break;
+        case 1:
+            dayStr = "Tuesday";
+            break;
+        case 2:
+            dayStr = "Wednesday";
+            break;
+        case 3:
+            dayStr = "Thursday";
+            break;
+        case 4:
+            dayStr = "Friday";
+            break;
+        case 5:
+            dayStr = "Saturday";
+            break;
+        default:
+            //We should technicaly never get here.
+            break;
+    }
 
     return (
         <ScrollView>
-            <Period title={day.firstPeriod.Title} room={day.firstPeriod.Room} time="08:00" />
-            <Period title={day.secondPeriod.Title} room={day.secondPeriod.Room} time="10:00" />
-            <Period title={day.thirdPeriod.Title} room={day.thirdPeriod.Room} time="13:30" />
-            <Period title={day.fourthPeriod.Title} room={day.fourthPeriod.Room} time="15:30" />
-            <Period title={day.fifthPeriod.Title} room={day.fifthPeriod.Room} time="18:30" />
-            <Period title={day.sixthPeriod.Title} room={day.sixthPeriod.Room} time="19:30" />
+            <Text style={dayStyle}>{dayStr}</Text>
+            <Period title={day.firstPeriod.Title} room={day.firstPeriod.Room} time="08:00-10:00" />
+            <Period title={day.secondPeriod.Title} room={day.secondPeriod.Room} time="10:00-12:00" />
+            <Period title={day.thirdPeriod.Title} room={day.thirdPeriod.Room} time="13:30-15:30" />
+            <Period title={day.fourthPeriod.Title} room={day.fourthPeriod.Room} time="15:30-17:30" />
+            <Period title={day.fifthPeriod.Title} room={day.fifthPeriod.Room} time="18:30-19:30" />
+            <Period title={day.sixthPeriod.Title} room={day.sixthPeriod.Room} time="19:30-20:30" />
         </ScrollView>
     );
 };
@@ -29,6 +56,11 @@ const styles = StyleSheet.create({
     },
     roomNumberStyle: {
         alignSelf: 'flex-end'
+    },
+    dayStyle: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        marginLeft: 5
     },
     thumbnailContainerStyle: {
         justifyContent: 'center',
--- src/screens/ScheduleScreen.windows.js
+++ src/screens/ScheduleScreen.windows.js
@@ -1,35 +1,184 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import { View, Text, StyleSheet, FlatList, AsyncStorage, Alert, ScrollView, ActivityIndicator, Dimensions } from 'react-native';
+// import AsyncStorage from '@react-native-community/async-storage';
+import DayViewComponent from '../components/DayViewComponent';
 //import Axios from 'axios';
 
 export default class ScheduleScreen extends Component {
-    state = { batch: "", schedule: [], courses: [] };
+    state = { batch: "", schedule: [], courses: [], isFetching: false, day: new Date().getDay() > 6 ? 0 : new Date().getDay() - 1 };
 
     async componentWillMount() {
         console.log('Mounting ScheduleScreen for Windows');
 
-        this.setState({ batch: await AsyncStorage.getItem("@batch") });
+        await this.getSchedule();
         console.log(this.state.batch == null ? "No Batch" : "ScheduleScreen Batch: " + this.state.batch);
         //FIXME: Axios isn't currently supported by rn-windows. So re-add that when it is.
-        return fetch('https://class-schedule.herokuapp.com/schedule/get/' + this.state.batch)
-            .then(response => response.json())
-            .then(responseJson => {
-                console.log(responseJson);
-                console.log(responseJson.schedule);
-                console.log(responseJson.courses);
-            })
-            .catch(err => {
-                console.log(err);
-            });
+        if (this.state.schedule[0] == null) {
+            return fetch('https://class-schedule.herokuapp.com/schedule/get/' + this.state.batch)
+                .then(response => response.json())
+                .then(async responseJson => {
+                    console.log(responseJson);
+                    console.log(responseJson.schedule);
+                    console.log(responseJson.courses);
+                    if (responseJson.message == "OK") {
+                        await AsyncStorage.setItem("@schedule", JSON.stringify(responseJson.schedule));
+                        await AsyncStorage.setItem("@courses", JSON.stringify(responseJson.courses));
+                        console.log("Finished donwloading and saving schedule");
+                        this.getSchedule();
+                    } else {
+                        Alert.alert("No Batch", "You entered an invalid batch. please enter the correct batch again");
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        } else {
+            console.log("Schedule exists so not downloading");
+        }
     }
 
     render() {
+        const { batch, schedule, day, isFetching } = this.state;
+        const { dayStyle, textStyle } = styles;
+
         return (
             <View>
-                <Text style={styles.textStyle}>{this.state.batch} For Windows</Text>
+                <Text style={textStyle}>{batch} For Windows</Text>
+                <FlatList
+                    ref={(ref) => this.flatListRef = ref}
+                    data={schedule}
+                    // getItemLayout={(data, index) => { return { length: 6, index, offset: 6 * index } }}
+                    horizontal
+                    pagingEnabled
+                    refreshing={isFetching}
+                    onRefresh={() => this.onRefresh()}
+                    initialScrollIndex={day}
+                    getItemLayout={(data, index) => ({ length: Dimensions.get('window').width - 10, offset: (Dimensions.get('window').width) * index, index })}
+                    onScrollToIndexFailed={({ }) => { this.flatListRef.scrollToIndex({ animated: true, index: 4 }) }}
+                    ListEmptyComponent={() => {
+                        return (
+                            <View style={{ flex: 1, flexDirection: 'row' }}>
+                                <Text>Loading Schedule...</Text>
+                                <ActivityIndicator size="large" />
+                            </View>
+                        )
+                    }}
+                    renderItem={({ item , index}) => {
+                        return (<View>
+                            <DayViewComponent day={item} index={index} dayNum={index}/>
+                        </View>
+                        );
+                    }} />
+                {/* * NOTE: ScrollView is much less buggy than the FlatList.
+                            - FlatList has an issue where the item is mid transition and just stays there and is misaligned. Stay with ScrollView
+                <ScrollView
+                    horizontal
+                    pagingEnabled
+                    showsHorizontalScrollIndicator={false}>
+                    {
+                        this.state.schedule.map((item) => {
+                            return <DayViewComponent day={item} key={item.id} />
+                        })
+                    }
+                </ScrollView> */}
             </View>
         );
+    }
+
+    onRefresh() {
+        this.setState({ isFetching: true }, () => this.forgetSchedule());
+    }
+
+    async getSchedule() {
+        let schedStr = ""
+        schedStr = await AsyncStorage.getItem("@schedule");
+        let batch = await AsyncStorage.getItem("@batch");
+        let crsStr = ""
+        crsStr = await AsyncStorage.getItem("@courses");
+        if (batch == null) {
+            this.props.navigation.replace("Login");
+        }
+        if (schedStr == null) {
+            console.log("Schedule string not found");
+            this.setState({ batch });
+            return;
+        }
+        coursArr = JSON.parse(crsStr);
+        let schedObj = JSON.parse(schedStr);
+        let schedArr = [];
+        schedArr.push(schedObj.monday);
+        schedArr.push(schedObj.tuesday);
+        schedArr.push(schedObj.wednesday);
+        schedArr.push(schedObj.thursday);
+        schedArr.push(schedObj.friday);
+        schedArr.push(schedObj.saturday);
+        for (let i = 0; i < schedArr.length; i++) {
+            if (schedArr[i].id == null) {
+                schedArr[i].id = i.toString();
+            }
+        }
+        console.log(schedArr[0]);
+
+        let date = new Date();
+        let day = date.getDay() > 6 ? 0 : date.getDay() - 1;
+        this.setState({ schedule: schedArr, courses: coursArr, batch, day});
+    }
+
+    async forgetSchedule() {
+        let tempSched = await AsyncStorage.getItem("@schedule");
+        let tempCourse = await AsyncStorage.getItem("@courses");
+
+        //TODO: Try to not remove
+        // await AsyncStorage.removeItem("@schedule");
+        // await AsyncStorage.removeItem("@courses");
+        fetch('https://class-schedule.herokuapp.com/schedule/get/' + this.state.batch)
+            .then(response => response.json())
+            .then(async responseJson => {
+                console.log(responseJson);
+                console.log(responseJson.schedule);
+                console.log(responseJson.courses);
+                if (responseJson.message == "OK") {
+                    await AsyncStorage.multiSet([['@schedule', JSON.stringify(responseJson.schedule)],
+                    ['@courses', JSON.stringify(responseJson.courses)]]);
+                    console.log("Finished donwloading and saving schedule");
+                } else {
+                    Alert.alert("No Batch", "The batch file is missing from the server. Using old data");
+                    await AsyncStorage.multiSet([['@schedule', tempSched], ['@courses', tempCourse]]);
+                }
+            })
+            .catch(async err => {
+                console.log(err);
+                Alert.alert("Network Error", "There was a network error so using stale data");
+                await AsyncStorage.multiSet([['@schedule', tempSched], ["@courses", tempCourse]], async (e) => {
+                    if (e) throw e;
+                    console.log("Refresh schedule failed. Network Error but re-setting schedule and courses was successful\n" + tempSched);
+                });
+            });
+
+        // Axios.get("https://class-schedule.herokuapp.com/schedule/get/" + this.state.batch)
+        //     .then(async res => {
+        //         if (res.data.message == "OK") {
+        //             const schedule = response.data.schedule;
+        //             const courses = response.data.courses;
+        //             await AsyncStorage.setItem("@schedule", JSON.stringify(schedule));
+        //             await AsyncStorage.setItem("@courses", JSON.stringify(courses));
+        //         } else {
+        //             Alert.alert("Error", "Did not find the batch so using stale data");
+        //             await AsyncStorage.multiSet([['@schedule', tempSched], ["@courses", tempCourse]])
+        //         }
+        //         await this.getSchedule();
+        //     })
+        //     .catch(async (e) => {
+        //         console.log(e);
+        //         Alert.alert("Error", "There was a network error so using stale data");
+        //         await AsyncStorage.multiSet([["@schedule", tempSched], ["@courses", tempCourse]], async (e) => {
+        //             if (e) throw e;
+        //             console.log("Refresh schedule failed. Network Error but re-setting schedule and courses was successful\n" + tempSched);
+        //             await this.getSchedule();
+        //         });
+        //     });
+        await this.getSchedule();
+        this.setState({ isFetching: false });
     }
 }
 
